d3be013ce526fc0cfb59c11e942db59900a86ca2
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-10-13 22:16:26 -0400

    Add other dependencies to base image

--- Dockerfile.cmake
+++ Dockerfile.cmake
@@ -10,3 +10,4 @@ RUN cmake --version
 
 RUN apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install build-essential software-properties-common -y && add-apt-repository ppa:ubuntu-toolchain-r/test -y && apt-get update -y && apt-get install gcc-11 g++-11 -y && update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11 && update-alternatives --config gcc
 
+RUN apt-get install -y git perl openssle libgmp3-dev
