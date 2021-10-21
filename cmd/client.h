fb2e4775ff82dd23b7d2a781a5a18329e035ed7d
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-10-21 09:20:37 -0400

    Add tls client headers

---
+++ cmd/client.h
@@ -0,0 +1,40 @@
+//
+// Created by Marcos Maceo on 10/20/21.
+//
+
+#ifndef SILKWORM_CLIENT_H
+#define SILKWORM_CLIENT_H
+
+#pragma once
+#include <iostream>                     // Used for cout
+
+// GLOBAL DEFINES
+#define PORT 6969
+#define MSG_2 "Yo client get the fuck off!"
+
+
+// LOGS
+#define LOG_I std::cout                 // Start log
+#define END_I std::endl                 // End log
+
+#define LOG_S LOG_I << "\033[92m"       // Success green log
+#define END_S "\033[37m" << END_I       // End success green log
+
+#define LOG_W LOG_I << "\033[33m"       // Warning orange log
+#define END_W "\033[37m" << END_I       // End warning orange log
+
+#define LOG_E LOG_I << "\033[31m"       // Error red log
+#define END_E "\033[37m" << END_I       // End Error red log
+
+// UTILS
+#define LOG_SSL_STACK() {\
+char buf[256];\
+\
+while (ERR_peek_error()) {\
+ERR_error_string_n(ERR_get_error(), buf, sizeof(buf));\
+LOG_E << "openssl error: " << buf << END_E;\
+}\
+}
+
+
+#endif  // SILKWORM_CLIENT_H
