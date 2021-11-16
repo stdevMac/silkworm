e2886f8ac7cde5a6eabb28a31dcad480958291df
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-11-16 16:57:13 +0000

    Update dockerfile for mbedtls

--- Dockerfile.cmake
+++ Dockerfile.cmake
@@ -8,6 +8,6 @@ RUN sh /cmake-3.21.3-linux-x86_64.sh --prefix=/opt/cmake --skip-license
 RUN ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 RUN cmake --version
 
-RUN apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install build-essential software-properties-common -y && add-apt-repository ppa:ubuntu-toolchain-r/test -y && apt-get update -y && apt-get install gcc-11 g++-11 -y && update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11 && update-alternatives --config gcc
+RUN apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install build-essential software-properties-common libmbedtls-dev -y && add-apt-repository ppa:ubuntu-toolchain-r/test -y && apt-get update -y && apt-get install gcc-11 g++-11 -y && update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11 && update-alternatives --config gcc
 
 RUN apt-get install -y git perl openssl libgmp3-dev
